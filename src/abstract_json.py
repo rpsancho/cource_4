7572f4cf4299610848d365821e5c2c156726e21e
Alexander Polikarpov <alex-polikarpov@yandex.ru>
2023-09-17 20:49:13 +0700

    refactor (change names), add get_vacancies_from_json_file method

--- src/abstract_json.py
+++ src/abstract_json.py
@@ -6,7 +6,3 @@ class AbstractJSON(ABC):
     @abstractmethod
     def save_vacancies_to_json_file(self, items: list):
         pass
-
-    @abstractmethod
-    def sort_vacancies(self, parameter, order):
-        pass

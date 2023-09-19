cb883c8b76bbddf3b1300026d6818e51c0d95bfa
Alexander Polikarpov <alex-polikarpov@yandex.ru>
2023-09-19 07:19:23 +0700

    move main logic into utils.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,70 +1,31 @@
-from src.head_hunter_api import HeadHunterAPI
-from src.super_job_api import SuperJobAPI
-from src.json_file_manager import JSONFileManager
-from src.vacancy import Vacancy
-from src.utils import is_string_in_dict
+from src.utils import search_vacancies, filter_vacancies, sort_vacancies, add_vacancy_to_file
 
 
 def main():
-    form = {}
 
-    user_input = input(f'Введите ключевые слова для поиска вакансий\n')
-    form['keywords'] = user_input.strip()
-
-    user_input = input(f'Введите минимальную зарплату в рублях\n')
-    form['payment_from'] = int(user_input.strip())
-
-    hh = HeadHunterAPI()
-    sj = SuperJobAPI()
-
-    hh_vacancies = hh.get_vacancies(form['keywords'], form['payment_from'])
-    sj_vacancies = sj.get_vacancies(form['keywords'], form['payment_from'])
-
-    # спрятать в функцию
     vacancy_list = []
-    for item in hh_vacancies['items']:
-        vacancy = Vacancy(
-            item['name'],
-            item['alternate_url'],
-            item['salary']['from'],
-            item['snippet']['responsibility'],
-            item['snippet']['requirement']
-        )
-        vacancy_list.append(vacancy)
-    for item in sj_vacancies['objects']:
-        vacancy = Vacancy(
-            item['profession'],
-            item['link'],
-            item['payment_from'],
-            item['work'],
-            item['candidat']
-        )
-        vacancy_list.append(vacancy)
 
-        saver = JSONFileManager()
-        saver.save_vacancies_to_json_file(vacancy_list)
-        saver.get_vacancies_from_json_file()
+    while True:
+        print("1 - поиск вакансий\n"
+              "2 - фильтрация по ключевому слову\n"
+              "3 - сортировка по зарплате\n"
+              "4 - сохранение в файл\n"
+              "5 - выход")
 
-        print(f'Список вакансий, удовлетворяющих запросу, получен и сохранен.')
-        user_input = input(f'Выберите дальнейшее действие:\n'
-                           f'1  -  cортировать вакансии по зарплате\n'
-                           f'2  -  фильтровать вакансии по дополнительному ключевому слову\n')
-        user_input = user_input.strip()
-        if user_input == '1':
-            vac_list = saver.get_vacancies_from_json_file()
-            sorted_vacancies = sorted(vac_list, key=lambda x: x['payment'], reverse=True)
-            for vac in sorted_vacancies:
-                print(f"{vac['title']}  {vac['payment']}  {vac['url']}")
-        elif user_input == '2':
-            vac_list = saver.get_vacancies_from_json_file()
-            user_input = input(f'введите ключевое слово:\n')
-            user_input = user_input.strip()
-            filtered_vacancies = [item for item in vac_list if is_string_in_dict(user_input, item)]
-            for vac in filtered_vacancies:
-                print(f"{vac['title']}  {vac['payment']}  {vac['url']}")
+        user_cmd = input().strip()
+
+        if user_cmd == '1':
+            search_vacancies(vacancy_list)
+        elif user_cmd == '2':
+            filter_vacancies(vacancy_list)
+        elif user_cmd == '3':
+            sort_vacancies(vacancy_list)
+        elif user_cmd == '4':
+            add_vacancy_to_file(vacancy_list)
+        elif user_cmd == '5':
+            return
         else:
-            print(f'непонятно')
-        return
+            print('Неизвестная команда\n')
 
 
 if __name__ == '__main__':
--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,86 @@
-def is_string_in_dict(s: str, d: dict):
-    return s in d['title'] or s in d['description'] or s in d['requirements']
+from src.head_hunter_api import HeadHunterAPI
+from src.super_job_api import SuperJobAPI
+from src.json_file_manager import JSONFileManager
+
+messages = {
+    'search_msg': 'Список вакансий получен',
+    'filter_msg': 'Вакансии отфильтрованы по ключевому слову',
+    'sort_msg': 'Вакансии отфильтрованы по ключевому слову'
+}
+
+
+def search_vacancies(vacancy_list):
+    user_input = input(f'Введите ключевые слова для поиска вакансий\n')
+    keywords = user_input.strip()
+
+    user_input = input(f'Введите минимальную зарплату в рублях\n')
+    salary = int(user_input.strip())
+
+    hh = HeadHunterAPI()
+    sj = SuperJobAPI()
+
+    vacancy_list.clear()
+    vacancy_list.extend(hh.get_vacancies(keywords, salary))
+    vacancy_list.extend(sj.get_vacancies(keywords, salary))
+
+    # saver = JSONFileManager()
+    # saver.save_vacancies_to_json_file(vacancy_list)
+
+    user_query(messages['search_msg'], print_vacancies, vacancy_list)
+
+
+def filter_vacancies(vacancy_list: list):
+    if not vacancy_list:
+        print('Результаты поиска отсутствуют. Сначала произведите поиск\n')
+        return
+
+    keywords = input(f'Введите ключевое слово для фильтрации вакансий\n').strip()
+
+    filtered_vacancy_list = [item for item in vacancy_list if item.is_str_in_attr(keywords)]
+    vacancy_list.clear()
+    vacancy_list.extend(filtered_vacancy_list)
+    user_query(messages['filter_msg'], print_vacancies, filtered_vacancy_list)
+
+
+def sort_vacancies(vacancy_list: list):
+    if not vacancy_list:
+        print('Результаты поиска отсутствуют. Сначала произведите поиск\n')
+        return
+
+    sorted_vacancy_list = sorted(vacancy_list, key=lambda x: x.payment, reverse=True)
+    vacancy_list.clear()
+    vacancy_list.extend(sorted_vacancy_list)
+    user_query(messages['sort_msg'], print_vacancies, sorted_vacancy_list)
+
+
+def user_query(message, action, action_arg):
+    while True:
+        print(f'{message}. Вывести результат? (y/n)\n')
+        user_cmd = input().lower().strip()
+        if user_cmd == 'y':
+            action(action_arg)
+            return True
+        elif user_cmd == 'n':
+            return False
+        else:
+            print('Неизвестная команда')
+            continue
+
+
+def print_vacancies(vacancy_list: list):
+    for count, vacancy in enumerate(vacancy_list):
+        print(f"{count}  {vacancy.title}  {vacancy.payment}  {vacancy.url}\n")
+
+
+def add_vacancy_to_file(vacancy_list: list):
+    if not vacancy_list:
+        print('Результаты поиска отсутствуют. Сначала произведите поиск\n')
+        return
+    pos = input(f'Введите номер сохраняемой вакансии\n').strip()
+    pos = int(pos)
+    saver = JSONFileManager()
+    try:
+        saver.save_vacancy_to_json_file(vacancy_list[pos])
+        print('Вакансия сохранена')
+    except IndexError:
+        print('Некорректный номер')

8c8e9794583c8fdd83918fdecd3c173477479c6a
Alexander Polikarpov <alex-polikarpov@yandex.ru>
2023-09-19 07:18:33 +0700

    create vacancy list inside get_vacancies method

--- src/super_job_api.py
+++ src/super_job_api.py
@@ -1,16 +1,14 @@
 import os
 import requests
 from src.abstract_api import AbstractAPI
+from src.vacancy import Vacancy
 
 
 class SuperJobAPI(AbstractAPI):
 
     __API_KEY = os.getenv('SUPER_JOB_API_KEY')
 
-    def __init__(self):
-        pass
-
-    def get_vacancies(self, keyword: str, payment: int):
+    def get_vacancies(self, keyword: str, payment: int) -> list:
 
         params = {
             'count': 100,
@@ -22,5 +20,18 @@ class SuperJobAPI(AbstractAPI):
             'X-Api-App-Id': SuperJobAPI.__API_KEY
         }
 
-        response = requests.get('https://api.superjob.ru/2.0/vacancies/', params=params, headers=headers)
-        return response.json()
+        response = requests.get('https://api.superjob.ru/2.0/vacancies/', params=params, headers=headers).json()
+
+        vacancy_list = []
+        for item in response['objects']:
+            vacancy = Vacancy(
+                item['profession'],
+                item['link'],
+                item['payment_from'],
+                item['work'],
+                item['candidat'],
+                'SJ'
+            )
+            vacancy_list.append(vacancy)
+
+        return vacancy_list
